16c3991cd8041f121881e1f7a2b3956c6cbb12e0
christop <christoph@lafkon.net>
2015-01-06 22:58:51 +0100

    respect spaces and newlines

--- var/labels/continous2pdfs.sh
+++ var/labels/continous2pdfs.sh
@@ -32,7 +32,7 @@
   if [ `ls $OUTDIR/${BASENAME}-* | wc -l` -gt 0 ]; then
        echo "export for $SVG does exist!"
        read -p "overwrite $OUTDIR/${BASENAME}-XX? [y/n] " ANSWER
-       if [ $ANSWER != y ] ; then echo "Bye"; exit 0; fi
+       if [ X$ANSWER != Xy ] ; then echo "Bye"; exit 0; fi
   fi
 
 
@@ -49,20 +49,25 @@
         EXPORTPAGES=120
   fi
 
+
+  BREAKFOO=NL${RANDOM}F00
+  SPACEFOO=SP${RANDOM}F0O
 # --------------------------------------------------------------------------- #
-# MOVE ALL LAYERS ON SEPARATE LINES IN A TMP FILE
+# MOVE ALL LAYERS ON SEPARATE LINES (TEMPORARILY; EASIFY PARSING LATER ON)
 # --------------------------------------------------------------------------- #
-  sed ':a;N;$!ba;s/\n/ /g' $SVG          | # REMOVE ALL LINEBREAKS
+  sed ":a;N;\$!ba;s/\n/$BREAKFOO/g" $SVG | # REMOVE ALL LINEBREAKS (BUT SAVE)
+  sed "s/ /$SPACEFOO/g"                  | # REMOVE ALL SPACE (BUT SAVE)
   sed 's/<g/4Fgt7RfjIoPg7/g'             | # PLACEHOLDER FOR GROUP OPEN
+  sed ':a;N;$!ba;s/\n/ /g'               | # REMOVE ALL NEW LINES
   sed 's/4Fgt7RfjIoPg7/\n<g/g'           | # RESTORE GROUP OPEN + NEWLINE
   sed '/groupmode="layer"/s/<g/4Fgt7R/g' | # PLACEHOLDER FOR LAYERGROUP OPEN
   sed ':a;N;$!ba;s/\n/ /g'               | # REMOVE ALL LINEBREAKS
   sed 's/4Fgt7R/\n<g/g'                  | # RESTORE LAYERGROUP OPEN + NEWLINE
+  grep -v 'inkscape:label="XX_'          | # REMOVE EXCLUDED LAYERS
+  sed 's/<\/svg>//g'                     | # REMOVE SVG CLOSE
   sed 's/display:none/display:inline/g'  | # MAKE VISIBLE EVEN WHEN HIDDEN
-  grep -v 'label="XX_'                   | # REMOVE EXCLUDED LAYERS
-  sed 's/<\/svg>/\n&/g'                  | # CLOSE TAG ON SEPARATE LINE
-  tr -s ' '                              | # CLEAN CONSECUTIVE SPACES
   tee > ${SVG%%.*}.tmp                     # WRITE TO TEMPORARY FILE
+
 
 # --------------------------------------------------------------------------- #
 # SHIFT CONTNENT AND EXPORT PDF FILES 
@@ -86,35 +91,40 @@
 
        TRANSFORM="transform=\"translate(-${SHIFTSUM},0)\""
 
-       echo $SVGHEADER              >  tmp.svg
+       echo $SVGHEADER        | # THE HEADER
+       sed "s/$BREAKFOO/\n/g" | # RESTORE ORIGINAL LINEBREAKS
+       sed "s/$SPACEFOO/ /g"  | # RESTORE ORIGINAL SPACES
+       tee                                                >  tmp.svg
        echo "<g $TRANSFORM inkscape:label=\"ALL\">"       >> tmp.svg  
-       grep "^<g" ${SVG%%.*}.tmp    >> tmp.svg
-       echo "</g>"                  >> tmp.svg
-       echo "</svg>"                >> tmp.svg
+       grep "^<g" ${SVG%%.*}.tmp | # SVG LAYERS
+       sed "s/$BREAKFOO/\n/g"    | # RESTORE ORIGINAL LINEBREAKS
+       sed "s/$SPACEFOO/ /g"     | # RESTORE ORIGINAL SPACES
+       tee                                                >> tmp.svg
+       echo "</g>"                                        >> tmp.svg
+       echo "</svg>"                                      >> tmp.svg
        
        inkscape --export-pdf=$PDF \
+                --export-text-to-path \
                 tmp.svg
-
-       gs                                     \
-         -o ${PDF%%.*}_CONFORMED.pdf          \
-         -sDEVICE=pdfwrite                    \
-         -sColorConversionStrategy=Gray       \
-         -sProcessColorModel=DeviceGray       \
-         -sColorImageDownsampleThreshold=2    \
-         -sColorImageDownsampleType=Bicubic   \
-         -sColorImageResolution=300           \
-         -sGrayImageDownsampleThreshold=2     \
-         -sGrayImageDownsampleType=Bicubic    \
-         -sGrayImageResolution=300            \
-         -sMonoImageDownsampleThreshold=2     \
-         -sMonoImageDownsampleType=Bicubic    \
-         -sMonoImageResolution=1200           \
-         -dSubsetFonts=true                   \
-         -dEmbedAllFonts=true                 \
-         -dAutoRotatePages=/None              \
-         -sCannotEmbedFontPolicy=Error        \
-         -c ".setpdfwrite<</NeverEmbed[ ]>> setdistillerparams" \
-         -f $PDF
+       gs -o ${PDF%%.*}_CONFORMED.pdf          \
+          -sDEVICE=pdfwrite                    \
+          -sColorConversionStrategy=Gray       \
+          -sProcessColorModel=DeviceGray       \
+          -sColorImageDownsampleThreshold=2    \
+          -sColorImageDownsampleType=Bicubic   \
+          -sColorImageResolution=300           \
+          -sGrayImageDownsampleThreshold=2     \
+          -sGrayImageDownsampleType=Bicubic    \
+          -sGrayImageResolution=300            \
+          -sMonoImageDownsampleThreshold=2     \
+          -sMonoImageDownsampleType=Bicubic    \
+          -sMonoImageResolution=1200           \
+          -dSubsetFonts=true                   \
+          -dEmbedAllFonts=true                 \
+          -dAutoRotatePages=/None              \
+          -sCannotEmbedFontPolicy=Error        \
+          -c ".setpdfwrite<</NeverEmbed[ ]>> setdistillerparams" \
+          -f $PDF
 
        mv ${PDF%%.*}_CONFORMED.pdf $PDF
 
