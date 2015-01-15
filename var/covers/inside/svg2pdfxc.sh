68b66520d622d612def3f9cf9df7a758b736a985
christop <christoph@lafkon.net>
2015-01-15 12:53:40 +0100

    export pdf files from inkscape that pdf-x compliant

---
+++ var/covers/inside/svg2pdfxc.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------------- #
+#                                                                             #
+#  Copyright (C) 2015 LAFKON/Christoph Haag                                   #
+#                                                                             #
+#  svg2pdfxc.sh is free software: you can redistribute it                     # 
+#  and/or modify it under the terms of the GNU General Public License as      # 
+#  published by the Free Software Foundation, either version 3,               #
+#  or (at your option) any later version.                                     #
+#                                                                             #
+#  svg2pdfxc.sh is distributed in the hope that it                            #
+#  will be useful, but WITHOUT ANY WARRANTY; without even the implied         #
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           #
+#  See the GNU General Public License for more details.                       #
+#                                                                             #
+# --------------------------------------------------------------------------- #
+
+  SVG=$1
+  PDF=${SVG%%.*}.pdf
+
+# --------------------------------------------------------------------------- #
+# INTERACTIVE CHECKS 
+# --------------------------------------------------------------------------- #
+  if [ ! -f ${SVG%%.*}.svg ]; then echo; echo "We need a svg!"
+                                         echo "e.g. $0 yoursvg.svg"; echo
+      exit 0;
+  fi
+  if [ -f $PDF ]; then
+       echo "export for $SVG does exist!"
+       read -p "overwrite ${PDF}? [y/n] " ANSWER
+       if [ X$ANSWER != Xy ] ; then echo "Bye"; exit 0; fi
+  fi
+# --------------------------------------------------------------------------- #
+
+  inkscape --export-pdf=$PDF \
+           --export-text-to-path \
+           $SVG
+
+  gs -o ${PDF%%.*}_CONFORMED.pdf          \
+     -sDEVICE=pdfwrite                    \
+     -sColorConversionStrategy=Gray       \
+     -sProcessColorModel=DeviceGray       \
+     -sColorImageDownsampleThreshold=2    \
+     -sColorImageDownsampleType=Bicubic   \
+     -sColorImageResolution=300           \
+     -sGrayImageDownsampleThreshold=2     \
+     -sGrayImageDownsampleType=Bicubic    \
+     -sGrayImageResolution=300            \
+     -sMonoImageDownsampleThreshold=2     \
+     -sMonoImageDownsampleType=Bicubic    \
+     -sMonoImageResolution=1200           \
+     -dSubsetFonts=true                   \
+     -dEmbedAllFonts=true                 \
+     -dAutoRotatePages=/None              \
+     -sCannotEmbedFontPolicy=Error        \
+     -c ".setpdfwrite<</NeverEmbed[ ]>> setdistillerparams" \
+     -f $PDF > /dev/null
+
+  mv ${PDF%%.*}_CONFORMED.pdf $PDF
+
+# --------------------------------------------------------------------------- #
+
+exit 0;
+
